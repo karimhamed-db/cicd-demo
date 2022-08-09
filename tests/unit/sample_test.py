b77660a2e62da49ec2a8afcffbb3192fbde66cd5
Karim Hamed <karim.hamed@databricks.com>
2022-08-09 18:35:26 +0200

    Add tests to sample job

--- tests/unit/sample_test.py
+++ tests/unit/sample_test.py
@@ -2,6 +2,7 @@ import unittest
 import tempfile
 import os
 import shutil
+import pandas as pd
 
 from cicd_sample_project.jobs.sample.entrypoint import SampleJob
 from pyspark.sql import SparkSession
@@ -9,7 +10,8 @@ from unittest.mock import MagicMock
 
 class SampleJobUnitTest(unittest.TestCase):
     def setUp(self):
-        self.test_dir = tempfile.TemporaryDirectory().name
+        tmp_dir = tempfile.TemporaryDirectory()
+        self.test_dir = tmp_dir.name
         self.spark = SparkSession.builder.master("local[1]").getOrCreate()
         self.test_config = {
             "output_format": "parquet",
@@ -17,11 +19,23 @@ class SampleJobUnitTest(unittest.TestCase):
         }
         self.job = SampleJob(spark=self.spark, init_conf=self.test_config)
 
-    def test_sample(self):
-        # feel free to add new methods to this magic mock to mock some particular functionality
-        self.job.dbutils = MagicMock()
+    def test_calculate_even_vs_odd(self):
+        expected = {
+            "isEven": [False, True], 
+            "count": [250, 250]
+        }
 
-        self.job.launch()
+        expected_df = pd.DataFrame(data=expected)
+
+        df = self.spark.range(0, 500)
+        actual_df = self.job.calculate_even_vs_odd(df).toPandas()
+
+        assert expected_df.equals(actual_df) is True
+
+    def test_write(self):
+        df = self.spark.range(0, 500)
+
+        self.job.write(df)
 
         output_count = (
             self.spark.read.format(self.test_config["output_format"])
@@ -32,7 +46,7 @@ class SampleJobUnitTest(unittest.TestCase):
         self.assertGreater(output_count, 0)
 
     def tearDown(self):
-        shutil.rmtree(self.test_dir)
+        shutil.rmtree(self.test_dir, ignore_errors=True)
 
 
 if __name__ == "__main__":
