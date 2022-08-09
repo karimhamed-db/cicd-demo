36135f2f0083abb08578caecf369d12a1ceca6a3
Karim Hamed <karim.hamed@databricks.com>
2022-08-09 18:35:26 +0200

    Add tests to sample job

--- cicd_sample_project/jobs/sample/entrypoint.py
+++ cicd_sample_project/jobs/sample/entrypoint.py
@@ -1,34 +1,45 @@
+from pyspark.sql.functions import col
+
 from cicd_sample_project.common import Job
 
 
 class SampleJob(Job):
     def init_adapter(self):
-        if not self.conf:
-            self.logger.info(
-                "Init configuration was not provided, using configuration from default_init method"
-            )
-            self.conf = {
-                "output_format": "delta",
-                "output_path": "dbfs:/dbx/tmp/test/interactive/cicd_sample_project",
-            }
-        else:
-            self.logger.info("Init configuration is already provided")
+        if self.conf:
+            return
+
+        self.logger.info(
+            "Init configuration was not provided, using configuration from default_init method"
+        )
+        self.conf = {
+            "output_format": "delta",
+            "output_path": "dbfs:/dbx/tmp/test/interactive/cicd_sample_project",
+        }
+
+    def read_data(self):
+        return self.spark.range(0, 1000)
+
+    def calculate_even_vs_odd(self, df):
+        return df.withColumn("isEven", col("id") % 2 != 0).groupBy("isEven").count()
+
+    def write(self, df):
+        (  
+            df
+                .write
+                .format(self.conf["output_format"])
+                .mode("overwrite")
+                .save(self.conf["output_path"])
+        )
 
     def launch(self):
         self.logger.info("Launching sample job")
 
-        listing = self.dbutils.fs.ls("dbfs:/")
+        df = self.read_data()
+        df = self.calculate_even_vs_odd(df)
+        
+        self.write(df)
 
-        for l in listing:
-            self.logger.info(f"DBFS directory: {l}")
-
-        df = self.spark.range(0, 1000)
-
-        df.write.format(self.conf["output_format"]).mode("overwrite").save(
-            self.conf["output_path"]
-        )
-
-        self.logger.info("Sample job finished!!")
+        self.logger.info("Sample job finished!")
 
 
 if __name__ == "__main__":
